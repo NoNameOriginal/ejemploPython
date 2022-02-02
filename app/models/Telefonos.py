95bacb14eb0612a2a6d2eaa97eb2ea6e22b01082
Natalia <caliope2794@gmail.com>
2022-02-02 16:12:38 -0500

    agregué los modales faltantes del update

--- app/models/Telefonos.py
+++ app/models/Telefonos.py
@@ -3,7 +3,7 @@ from . import *
 class Telefonos(db.Model):
     __tablename__ = 'telefonos'
     telefono = db.Column(db.String(30), primary_key=True)
-    sede_id = db.Column(db.Integer, ForeignKey('sedes.id'))
+    sede_id = db.Column(db.String(30), ForeignKey('sedes.id'))
     
     def __init__(self, datadict ):
         for key, value in datadict.items():
